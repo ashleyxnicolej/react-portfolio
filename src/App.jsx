9e136db2bb460236ee11d36f18a8377c474d452d
ashleyxnicolej <ashleyjackson439@gmail.com>
2025-02-14 19:21:07 -0500

    Added Header and Footer Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
+      <Header />
       <Navbar />
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
